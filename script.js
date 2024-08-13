2acf7d5526380ece18b72d6667d57c3b2219c679
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-13 10:18:23 +0300

    add Form & JS

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // Giphy API key : kuWVJypKKh14TSGyiUpCfnjhUldNPZDN
 
+document.getElementById('weatherForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+    
+    const location = document.getElementById('location').value;
 
-
-fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=KGK3U8EUGUP5LXBAT2NRZJ5LB', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response.currentConditions.temp);
-    })
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=KGK3U8EUGUP5LXBAT2NRZJ5LB`, { mode: 'cors' })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(`Current temperature in ${location}: ${data.currentConditions.temp}°C`);
+        })
+        .catch(function(error) {
+            console.error('Error fetching the weather data:', error);
+        });
+});
